91fd0845fa2c27aa1712f8de178ce761c42fd8e5
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-08-22 20:19:42 +0300

    Initial commit. set up the base of the app(spring boot with some dependencies and hibernate)

---
+++ src/main/java/com/example/photocontestproject/PhotoContestProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.photocontestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoContestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoContestProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/photocontestproject/PhotoContestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photocontestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoContestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
