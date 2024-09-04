ea4f804fd0d34f04a0bfcf3a99a34b91c3906d42
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-09-04 21:28:37 +0300

    Made a contest view fragment and used it in the home view with a sample of 3 random posts being displayed

--- src/main/java/com/example/photocontestproject/controllers/mvc/HomeMvcController.java
+++ src/main/java/com/example/photocontestproject/controllers/mvc/HomeMvcController.java
@@ -1,13 +1,29 @@
 package com.example.photocontestproject.controllers.mvc;
 
+import com.example.photocontestproject.enums.ContestPhase;
+import com.example.photocontestproject.models.Contest;
+import com.example.photocontestproject.services.contracts.ContestService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class HomeMvcController {
+    private final ContestService contestService;
+
+    public HomeMvcController(ContestService contestService) {
+        this.contestService = contestService;
+    }
+
+    @ModelAttribute("contests")
+    public List<Contest> populateContests() {
+        return contestService.getAllContests(null, null, null, ContestPhase.PhaseI).stream().limit(3).toList();
+    }
 
     @GetMapping
     public String getHomeView() {
