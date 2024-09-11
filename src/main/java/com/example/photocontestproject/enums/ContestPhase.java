941bc611bc29696b4ddcf8e9032d24c63db6c91c
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-09-11 20:45:52 +0300

    Improved presentation of contest phases

--- src/main/java/com/example/photocontestproject/enums/ContestPhase.java
+++ src/main/java/com/example/photocontestproject/enums/ContestPhase.java
@@ -3,5 +3,15 @@ package com.example.photocontestproject.enums;
 public enum ContestPhase {
     PhaseI,
     PhaseII,
-    Finished
+    Finished;
+
+
+    @Override
+    public String toString() {
+        return switch (this) {
+            case PhaseI -> "Phase I";
+            case PhaseII -> "Phase II";
+            case Finished -> "Finished";
+        };
+    }
 }
