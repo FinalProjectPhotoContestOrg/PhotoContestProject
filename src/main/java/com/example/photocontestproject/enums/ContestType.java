f8524cd23de6b772dab9f57cb17c61951fdea2b2
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-09-14 16:38:42 +0300

    Refactored Enums to better follow good coding principles and be more consistent

--- src/main/java/com/example/photocontestproject/enums/ContestType.java
+++ src/main/java/com/example/photocontestproject/enums/ContestType.java
@@ -1,6 +1,21 @@
 package com.example.photocontestproject.enums;
 
 public enum ContestType {
-    Open,
-    Invitational
+    Open("Open"),
+    Invitational("Invitational");
+
+    private final String name;
+
+    ContestType(String name) {
+        this.name = name;
+    }
+
+    public boolean equalsName(String otherName) {
+        return name.equals(otherName);
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
